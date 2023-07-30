3f29ce065951d4442471df242e9f73588737fd75
Noreh AD <akinoreh@gmail.com>
2023-07-30 17:08:49 +0300

    track controls

--- controls.js
+++ controls.js
@@ -28,6 +28,9 @@ window.addEventListener("AllScriptsLoaded", () => {
     controls.scrollbar.width  = qs("#scrollbar-width-input");
     controls.scrollbar.height = qs("#scrollbar-height-input");
     
+    controls["scrollbar-track"]["background-color"] = qs("#track-background-input");
+    controls["scrollbar-track"]["border-radius"]    = qs("#track-radius-input");
+    
     on([controls.scrollbar.width, qs("#scrollbar-width-range")], "input", function () {
         if (!["0", "", NaN].includes(this.value)) {
             css.scrollbar.width = this.value + "px";
@@ -39,9 +42,20 @@ window.addEventListener("AllScriptsLoaded", () => {
         }
     });
     
+    on([controls["scrollbar-track"]["background-color"]], "input", function () {
+        css["scrollbar-track"]["background-color"] = this.value;
+    });
+    on([controls["scrollbar-track"]["border-radius"], qs("#track-radius-range")], "input", function () {
+        if (!["0", "", NaN].includes(this.value)) {
+            css["scrollbar-track"]["border-radius"] = this.value + "px";
+        }
+    });
+    
     on([
         controls.scrollbar.width,  qs("#scrollbar-width-range"),
         controls.scrollbar.height, qs("#scrollbar-height-range"),
+        controls["scrollbar-track"]["background-color"],
+        controls["scrollbar-track"]["border-radius"], qs("#track-radius-range"),
     ], "input", function () {
         applyCSS();
         outputCSS();
@@ -49,7 +63,18 @@ window.addEventListener("AllScriptsLoaded", () => {
     
     syncNumberInputAndRange(controls.scrollbar.width, qs("#scrollbar-width-range"));
     syncNumberInputAndRange(controls.scrollbar.height, qs("#scrollbar-height-range"));
+    syncColorAndTextInput(controls["scrollbar-track"]["background-color"], qs("#track-background-text"));
+    syncNumberInputAndRange(controls["scrollbar-track"]["border-radius"], qs("#track-radius-range"));
 });
+
+function syncColorAndTextInput(color, textInput) {
+    color.addEventListener("input", () => {
+        textInput.value = color.value;
+    });
+    textInput.addEventListener("input", () => {
+        color.value = textInput.value;
+    });
+}
 
 function syncNumberInputAndRange(numberInput, range) {
     numberInput.addEventListener("input", () => {
